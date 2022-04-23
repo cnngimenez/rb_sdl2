dd003a311d119ef38a6cc8bd8519c6e92c4c9fed
shinokaro <shinokaro@hotmail.co.jp>
2022-04-23 13:43:10 +0900

    fix error

--- lib/rb_sdl2/error.rb
+++ lib/rb_sdl2/error.rb
@@ -5,15 +5,15 @@ module RbSDL2
       def clear = ::SDL.ClearError
 
       # SDL からのエラーメッセージを文字列で返します。
-      # SDL からエラーが通知されてもえらメッセージがあるとは限りません。
+      # SDL からエラーが通知されてもエラーメッセージがあるとは限りません。
       # SDL の関数はエラーの状態を示してもエラーメッセージをセットしない場合があります。
       def last_error_message = ::SDL.GetError.read_string
 
       def last_error_message=(error_message)
-        raise TypeError unless String === error_message
-        # SDL_SetError() の第一引数は sprintf フォーマットだが、第二引数以降に可変長引数を与える方法が無い。
-        # "%" をエスケープして第二引数を無視させてメモリー参照を行わないようにする。
-        ::SDL.SetError(error_message.gsub(/%/, "%%"))
+        # SDL_SetError() の第一引数は sprintf フォーマットである。
+        # このメソッドのデザインの都合上、可変長引数を与える方法が無い。
+        # "%" をエスケープすることで第二引数を無視させてメモリー参照を行わないようにする。
+        ::SDL.SetError(String.new(error_message, encoding: Encoding::UTF_8).gsub(/%/, "%%"))
       end
     end
   end
