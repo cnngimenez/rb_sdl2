efdd671dcbcf967e471174be778363284f4bf529
shinokaro <shinokaro@hotmail.co.jp>
2022-05-04 01:51:43 +0900

    change ref_count_pointer

--- lib/rb_sdl2/ref_count_pointer.rb
+++ lib/rb_sdl2/ref_count_pointer.rb
@@ -1,20 +1,35 @@
 module RbSDL2
   class RefCountPointer < ::FFI::AutoPointer
+    # 備考：SDL では参照カウンターを操作する際にロックを行っていない。
+    # SDL_AtomicAdd() を使用した理由はオブジェクトの生成をなるべく行わないようにするため。
     class << self
-      def entity_class
-        raise NotImplementedError
-      end
+      def entity_class = raise NotImplementedError
+
+      def offset_of_refcount = entity_class.offset_of(:refcount)
+
+      def dec_ref(ptr) = ::SDL.SDL_AtomicAdd(ptr + offset_of_refcount, -1)
+
+      def inc_ref(ptr) = ::SDL.SDL_AtomicAdd(ptr + offset_of_refcount, 1)
 
       def to_ptr(ptr)
-        raise ArgumentError, 'Invalid pointer, ptr is NULL' if ptr.null?
-        # refcount の増加を AutoPointer 化する前に成功させる必要がある。
-        # AutoPointer になった後に失敗すると、GC に回収されたとき refcount が実際の参照数より少なくなる。
-        entity_class.new(ptr)[:refcount] += 1
-        new(ptr)
+        obj = new(ptr)
+
+        # ポインターの参照カウントを増加する際に例外が発生すると、
+        # ポインターが GC に回収され refcount が実際の参照数より少なくなる。
+        # 例外を補足してポインターのファイナライザーを解除する。
+        unless ptr.null?
+          begin
+            inc_ref(ptr)
+          rescue => e
+            obj.autorelease = false
+            raise e
+          end
+        end
+
+        obj
       end
     end
 
-    # for debug
-    def refcount = self.class.entity_class.new(self)[:refcount]
+    def refcount = get_int(self.class.offset_of_refcount)
   end
 end
